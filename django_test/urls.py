1655d406802c61e63da24836887ed008c6776cac
Jinhyun Cho <jinhyun.cho@access-company.com>
2014-02-27 15:05:35 +0900

    modified into newbranche

--- django_test/urls.py
+++ django_test/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'django_test.views.home', name='home1'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
